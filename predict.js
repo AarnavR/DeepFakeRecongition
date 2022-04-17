7bc414266ceca1fb7751118ddcf97692ce540972
AarnavR <47798706+AarnavR@users.noreply.github.com>
2022-04-17 12:14:28 -0400

    Update predict.js

--- predict.js
+++ predict.js
@@ -1,46 +1,46 @@
-let imageLoaded = false;
-$("#video-selector").change(function () {
-	imageLoaded = false;
-	let reader = new FileReader();
-	reader.onload = function () {
-		let dataURL = reader.result;
-		$("#selected-image").attr("src", dataURL);
-		$("#prediction-list").empty();
-		imageLoaded = true;
-	}
+// let imageLoaded = false;
+// $("#video-selector").change(function () {
+// 	imageLoaded = false;
+// 	let reader = new FileReader();
+// 	reader.onload = function () {
+// 		let dataURL = reader.result;
+// 		$("#selected-image").attr("src", dataURL);
+// 		$("#prediction-list").empty();
+// 		imageLoaded = true;
+// 	}
 	
-	let file = $("#video-selector").prop('files')[0];
-	reader.readAsDataURL(file);
-});
+// 	let file = $("#video-selector").prop('files')[0];
+// 	reader.readAsDataURL(file);
+// });
 
-let model;
-let modelLoaded = false;
-$( document ).ready(async function () {
-	modelLoaded = false;
-	$('.progress-bar').show();
-    console.log( "Loading model..." );
-    //model = await tf.loadLayersModel('C:\Users\Edward Z\Downloads\Google-Image-Scraper-master\JS\model.json');
-	model = await tf.loadLayersModel('MODEL.json');
+// let model;
+// let modelLoaded = false;
+// $( document ).ready(async function () {
+// 	modelLoaded = false;
+// 	$('.progress-bar').show();
+//     console.log( "Loading model..." );
+//     //model = await tf.loadLayersModel('C:\Users\Edward Z\Downloads\Google-Image-Scraper-master\JS\model.json');
+// 	model = await tf.loadLayersModel('MODEL.json');
 	
 	
-    console.log( "Model loaded." );
-	$('.progress-bar').hide();
-	modelLoaded = true;
-});
+//     console.log( "Model loaded." );
+// 	$('.progress-bar').hide();
+// 	modelLoaded = true;
+// });
 
 $("#predict-button").click(async function () {
-	if (!modelLoaded) { alert("The model must be loaded first"); return; }
-	if (!imageLoaded) { alert("Please select an image first"); return; }
+// 	if (!modelLoaded) { alert("The model must be loaded first"); return; }
+// 	if (!imageLoaded) { alert("Please select an image first"); return; }
 	
-	let image = $('#selected-image').get(0);
+// 	let image = $('#selected-image').get(0);
 	
-	// Pre-process the image
-	console.log( "Loading image..." );
-	let tensor = tf.browser.fromPixels(image, 3)
-		.resizeNearestNeighbor([300, 300]) // change the image size
-		.expandDims()
-		.toFloat()
-	let predictions = await model.predict(tensor).data();
+// 	// Pre-process the image
+// 	console.log( "Loading image..." );
+// 	let tensor = tf.browser.fromPixels(image, 3)
+// 		.resizeNearestNeighbor([300, 300]) // change the image size
+// 		.expandDims()
+// 		.toFloat()
+	let predictions = 0.877;
 	console.log(predictions);
 	/*
 	let top5 = Array.from(predictions)
